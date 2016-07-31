924a6ef5327c26f6c48888791e4f8d986529b01e
weichen0827 <863697667@qq.com>
2016-07-31 22:12:19 +0800

    0731_Const

---
+++ Chapter02_VariablesAndBasicTypes/2_4_1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	// const int ci = 42;
+	// const int &r1 = ci;
+	// //int &r2 = ci;
+	// const int r2 = 1024;
+
+	// const int &r3 = 1;
+
+
+	double dval = 3.56;
+
+	const int &r = 4.444;
+
+	cout << r << endl
+	
+	;return 0;
+}
