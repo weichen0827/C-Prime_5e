4cc3ff4f4f093a0f711af436940764c4970a55d4
weichen0827 <863697667@qq.com>
2016-08-04 21:53:22 +0800

    const02

--- Chapter02_VariablesAndBasicTypes/2_4_1.cpp
+++ Chapter02_VariablesAndBasicTypes/2_4_1.cpp
@@ -15,7 +15,21 @@ int main(int argc, char const *argv[])
 
 	const int &r = 4.444;
 
-	cout << r << endl
-	
-	;return 0;
+	cout << r << endl;
+
+	double val = 3.14;
+	const double *p = &val;
+
+	int errNumb = 0;
+	int *const curErr = &errNumb;
+
+	const double pi = 3.14159;
+	const double *const pip = &pi;
+
+	*curErr = 1;
+	//*pip = 4.4;	//assignment of read-only location ‘*(const double*)pip’   
+	int test = 44;
+	//curErr = &test;	//assignment of read-only variable ‘curErr’
+
+	return 0;
 }
---
+++ Chapter02_VariablesAndBasicTypes/2_4_2.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	// int i = -1, &r = i;
+
+	// const int a = -1, &b = 0;
+
+	// int i2 = 0;
+	// const int *p1 = &i2; 
+
+	// const int i3 = i, &c = i;
+
+	// int *const p2 = &i2;
+
+	// const int *const p3 = &i2;
+
+	//const int &const r2 = a; // ‘const’ qualifiers cannot be applied to ‘const int&’
+	int a = 1;
+	//int &const r2 = a;
+
+	int i, *const cp = &i;
+
+	int *p1, *const p2 = p1;
+
+	const int ic = 0, &r = ic;
+
+	const int *const p3 = &i;
+
+	const int *p;
+
+	i = ic;
+	//p1 = p3; 	//invalid conversion from ‘const int*’ to ‘int*’ [-fpermissive]
+	//p1 = &ic;	//invalid conversion from ‘const int*’ to ‘int*’ [-fpermissive]
+	//p3 = &ic;	//assignment of read-only variable ‘p3
+	//p2 = p1;	//assignment of read-only variable ‘ic’
+	p1 = p2;
+	//ic = *p3;	//assignment of read-only variable ‘ic’
+
+
+	int ci = 1;
+	const int & aa = ci;
+	//aa = 3;	//assignment of read-only reference ‘aa’
+
+	return 0;
+}
